a54c8b210b68be6f0bce42bc017a50d6bee07d54
Harithya Wisesa <harithya77@gmail.com>
2025-05-05 20:20:41 +0700

    update title

--- pages/index.vue
+++ pages/index.vue
@@ -2,11 +2,11 @@
     <div class="bg-primary p-5 sticky top-0 z-10">
         <div class="relative">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                stroke="currentColor" class="size-6 absolute text-gray-400 top-3 ms-3">
+                stroke="currentColor" class="size-5 absolute text-gray-400 top-2.5 ms-3">
                 <path stroke-linecap="round" stroke-linejoin="round"
                     d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
             </svg>
-            <input type="text" class="bg-white w-full rounded-lg pl-14 px-5 py-3" placeholder="Find your pokemon" />
+            <input type="text" class="bg-white w-full rounded-lg pl-14 px-5 py-2" placeholder="Find your pokemon" />
         </div>
     </div>
 
@@ -59,5 +59,20 @@ onMounted(() => {
     if (observer.value) {
         intersectionObserver.observe(observer.value)
     }
+})
+
+// seo 
+useHead({
+    title: "Home | Pokedex",
+    meta: [
+        {
+            name: "description",
+            content: "Get information about your favorite pokemon"
+        },
+        {
+            name: "theme-color",
+            content: "#ef6467"
+        }
+    ]
 })
 </script>
